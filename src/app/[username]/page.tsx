8206eb1bf44bbb9af058ffef60dc1a3da09f86c3
gogleset <choij2494@gmail.com>
2025-04-13 11:43:39 +0900

    style: figma 동기화작업

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -141,7 +141,7 @@ export default function ResumeTemplate() {
   }, [username]);
 
   return (
-    <div className="min-h-screen p-6 md:p-10 bg-[#121212]">
+    <div className="min-h-screen p-6 md:p-10 ">
       <div className="max-w-4xl mx-auto">
         <div className="flex justify-between mb-6 items-center">
           <div className="flex items-center gap-2">
